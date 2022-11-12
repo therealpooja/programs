b0bf39a8f4b048290ecaeb9e19db0c8c22de8723
therealpooja <poojamktr20@gmail.com>
2022-11-12 16:29:47 +0530

    ADD

---
+++ list2.py
@@ -0,0 +1,8 @@
+#in the given list ,find value 2 ,if it is present,replace it with 200,only update the first occurance
+#Write a program to turn every tiem of the list into its square
+#output-[1,4,9,16,25]
+no=[1,2,3,4,5,2,6]
+indx=no.index(2)
+no[indx]=200
+print(no)
+    
