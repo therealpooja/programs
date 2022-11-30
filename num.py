7b352bf2081f5b00eb52d35496303639c90db797
therealpooja <poojamktr20@gmail.com>
2022-11-30 11:34:34 +0530

    ADD

---
+++ num.py
@@ -0,0 +1,5 @@
+num1=input("enter the first number:")
+num2=input("enter the second number:")
+num3=input("enter the third number:")
+#(int(num1)+int(num2)+int(num3))/3
+print(f"average of three numbers are:{(float(num1)+float(num2)+float(num3))/3}")
---
+++ tuple10.py
@@ -0,0 +1,8 @@
+tuple1=("car","bike","boat","jet","plane")
+(v1,v2,v3,v4,v5)=tuple1
+print(tuple1)
+print(v1)
+print(v2)
+print(v3)
+print(v4)
+print(v5)
