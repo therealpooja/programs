3a547283e315caf7bfe160a737cfa4e13634b5d9
therealpooja <poojamktr20@gmail.com>
2022-12-01 22:38:10 +0530

    GIT

---
+++ range11.py
@@ -0,0 +1,6 @@
+name=str(input("Tell your swwet name: "))
+age=int(input("enter your sweet age: "))
+if (name[0]=="a" or name[0]=="A") and age >=10:
+          print("You can watch coco movie")
+else:
+          print("sorry, you cannot watch the coco")
