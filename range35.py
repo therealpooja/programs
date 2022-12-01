383fd87690ddfe49c33bcda855b1ab37beec80fe
therealpooja <poojamktr20@gmail.com>
2022-12-02 00:26:03 +0530

    GIT

---
+++ range35.py
@@ -0,0 +1,9 @@
+name="Shanmukanath Reddy"
+age="18"
+print(f"your name is {name} and your age is {age}")
+
+
+
+name="Shanmukanath Reddy"
+age=18
+print("your name is {} and your age is {}".format(name,age))
