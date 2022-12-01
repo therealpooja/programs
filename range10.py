98d9e6251e01fb7f92ed5d00bd2ecf2f6381ce60
therealpooja <poojamktr20@gmail.com>
2022-12-01 22:36:03 +0530

    GIT

---
+++ range10.py
@@ -0,0 +1,9 @@
+age=int(input("Print your age: "))
+if age==1 and age<3:
+          print("Ticket price = free")
+elif age==4 and age<=10:
+          print("Ticket price = 150")
+elif age==11 and age<=60:
+          print("Ticket price = 250")
+else:
+          print("Ticket price = 200")
