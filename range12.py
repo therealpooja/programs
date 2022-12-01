b7c69447aa9f09b8909171d668e1a804883eec2d
therealpooja <poojamktr20@gmail.com>
2022-12-01 22:41:21 +0530

    GIT

---
+++ range12.py
@@ -0,0 +1,17 @@
+name="Shanmukanath Reddy"
+# indexing
+print(name[0])
+
+# sting slicing
+name="Shanmukanath Reddy"
+print(name[::-1])
+print(name[:-2])
+print(name[:3])
+# user input
+name=input("enter the given name:")
+# len functions,name.lower,name.upper
+name="Shanmukanath Reddy"
+print(len(name))
+print(name.upper)
+para="i am Shanmukanath Reddy and my age is 18"
+print(para.replace("Shanmukanath","Sh"))
