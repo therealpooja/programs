c608253153a567137a4e1e1d13bb1dceb0f74c2e
therealpooja <poojamktr20@gmail.com>
2022-12-01 23:49:44 +0530

    ENTER

---
+++ range20.py
@@ -0,0 +1,12 @@
+first_name="Shanmukanath Reddy"
+second_name="shannu"
+fullname=first_name+second_name
+print(fullname)
+a=45
+b=322
+print(a+b)
+
+
+a="Shanmukanath Reddy"
+print(a+"3")
+print(a * 3)
