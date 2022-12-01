b546d79bdd8ddc325ba8a1389985e2bc1bb25bfc
therealpooja <poojamktr20@gmail.com>
2022-12-02 00:00:24 +0530

    GET

---
+++ range25.py
@@ -0,0 +1,12 @@
+print("Shanmukanath \"world\" world")
+print('Shanmukanath is a very\n\t good boy')
+print("Shanmu\bkanath    \b  \b\bloves  Shanu")
+
+print("Line A \\n Line B")
+print("Line A \\t Line B")
+
+print("\\\\")
+print("/\\/\\/\\/\\/\\  mountains")
+
+print("he is     \b\b\bawesome")
+print(" \\\" ")
