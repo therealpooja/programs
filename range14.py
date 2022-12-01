1d910dc210c70396a518077aacba2e9e7226b009
therealpooja <poojamktr20@gmail.com>
2022-12-01 23:39:16 +0530

    GIT

---
+++ range14.py
@@ -0,0 +1,32 @@
+# and statements
+name="Shanmukanath Reddy"
+age=24
+if name=="Shanmukanath Reddy" and age==18:
+          print("Yes the thing which u have written is correct")
+else:
+          print("this is wrong ")
+
+
+name="Shanmukanath Reddy"
+age=18
+if name=="Shanmukanath Reddy" and age==18:
+          print("Yes the thing which u have written is correct")
+else:
+          print("this is wrong ")
+# or statements
+
+name="Shanmukanath Reddy"
+age=18
+if name=="Shanmukanath Reddy" or age==18:
+          print("Yes the thing which u have written is correct")
+else:
+          print("this is wrong ")
+
+
+
+name="khasim basha"
+age=24
+if name=="khasim basha" and age==24:
+          print("Yes the thing which u have written is correct")
+else:
+          print("this is wrong ")
