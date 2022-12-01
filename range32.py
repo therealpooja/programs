e2341dfcdb38c1a94b9bb1766940b6c2324a3861
therealpooja <poojamktr20@gmail.com>
2022-12-02 00:19:53 +0530

    GIT

---
+++ range32.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))
+print((a+b+c)/3)
+
+#or
+# string formatting
+
+a,b,c = int(input("")),int(input("")),int(input(""))
+print(f"The average of the three numbers are {(a+b+c)/3}")
