bab1e54a52f90b947d759f5a16d97d2366e57ea1
therealpooja <poojamktr20@gmail.com>
2022-12-04 12:35:24 +0530

    ADD

---
+++ myset2.py
@@ -0,0 +1,8 @@
+myset1={"car","bike","boat"}
+myset2={1,2,3}
+myset3={4,5,6,7}
+myset2.remove
+output=myset2.union(myset3)
+output1=myset2.intersection(myset3)
+output3=myset2.symmetric_difference(myset3)
+print(myset1)
